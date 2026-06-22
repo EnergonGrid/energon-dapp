d59832191b09d105f7123dffa939524a93458619
Cory Grannell <EnergonGrid@gmail.com>
2026-06-21 22:22:13 -0400

    Update Energon dApp favicon package

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,16 +5,16 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Primary favicon */}
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
 
-        {/* PNG fallbacks */}
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        {/* Modern favicon formats */}
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
 
         {/* Apple / iOS */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
-        {/* Android / PWA */}
+        {/* PWA / Manifest */}
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#05060b" />
 
