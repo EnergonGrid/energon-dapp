1e20de40dab15646306c38fc4538b7f9a48f8e78
Cory Grannell <EnergonGrid@gmail.com>
2026-08-01 10:57:24 -0400

    Standardize Energon favicon and PWA branding

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,23 +4,37 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Primary favicon */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico?v=3" sizes="any" />
 
-        {/* Modern favicon formats */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon.svg?v=3"
+        />
 
-        {/* Apple / iOS */}
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png?v=3"
+        />
 
-        {/* PWA / Manifest */}
-        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png?v=3"
+        />
+
+        <link
+          rel="manifest"
+          href="/site.webmanifest?v=3"
+        />
+
         <meta name="theme-color" content="#05060b" />
-
-        {/* App identity */}
         <meta name="application-name" content="Energon" />
-        <meta name="apple-mobile-web-app-title" content="Energon" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Energon"
+        />
       </Head>
 
       <body>
